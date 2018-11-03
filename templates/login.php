a17dd1c6614ed7bf0e4445e5f6549f900a240d65
Akash B <bobba.akash@gmail.com>
2018-11-03 14:57:28 -0400

    Update login.php

--- templates/login.php
+++ templates/login.php
@@ -5,30 +5,20 @@
    define('DB_DATABASE', 'presencedb');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
    session_start();
-   
-      // username and password sent from form 
-      
       $myusername = mysqli_real_escape_string($db,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
-      
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']);  
       $sql = "SELECT id FROM user WHERE username = '$myusername' and passcode = '$mypassword'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $active = $row['active'];
-      
-      $count = mysqli_num_rows($result);
-      
-      // If result matched $myusername and $mypassword, table row must be 1 row
-		
+      $count = mysqli_num_rows($result);	
       if($count == 1) {
           echo "naniii";
          session_register("myusername");
          $_SESSION['login_user'] = $myusername;
-         
          header("location: welcome.php");
       }else {
           echo "no";
          $error = "Your Login Name or Password is invalid";
       }
-   
 ?>

41a5115605d0decc85006996dd5777dd9e2f331c
Akash B <bobba.akash@gmail.com>
2018-11-03 16:16:15 -0400

    Create redirect.php

---
+++ templates/redirect.php
@@ -0,0 +1,8 @@
+function redirect($url){
+    if (headers_sent()){
+      die('<script type="text/javascript">window.location=\''.$url.'\';</script‌​>');
+    }else{
+      header('Location: ' . $url);
+      die();
+    }    
+}

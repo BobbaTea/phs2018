fbc5ee51abcab432d81b6cb7fbdfb71fc9804a7c
Akash B <bobba.akash@gmail.com>
2018-11-03 14:50:21 -0400

    Update login.php

--- templates/login.php
+++ templates/login.php
@@ -6,7 +6,6 @@
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
    session_start();
    
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form 
       
       $myusername = mysqli_real_escape_string($db,$_POST['username']);
@@ -31,5 +30,5 @@
           echo "no";
          $error = "Your Login Name or Password is invalid";
       }
-   }
+   
 ?>

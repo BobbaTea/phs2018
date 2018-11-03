52c8996902f8fd0b090e4aba2940faec6524a9c0
Akash B <bobba.akash@gmail.com>
2018-11-03 11:48:15 -0400

    Update update.php

--- admin/update.php
+++ admin/update.php
@@ -1,7 +1,11 @@
 <?php
 
-shell_exec("cd /var/www/html; rm -rf /var/www/html/*; git clone git@github.com:BobbaTea/phs2018.git; mv /var/www/html/phs2018/* /var/www/html; rm -rf /var/www/html/phs2018;");
+
+shell_exec("rm -rf /var/www/html/*; git clone git@github.com:BobbaTea/phs2018.git; mv /var/www/html/phs2018/* /var/www/html;");
+
 
 ?>
+
+
 
 

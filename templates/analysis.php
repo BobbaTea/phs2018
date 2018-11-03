9b62d5c185b74d5b598f33ff12e2c7056693323a
Akash B <bobba.akash@gmail.com>
2018-11-03 19:08:43 -0400

    Update analysis.php

--- templates/analysis.php
+++ templates/analysis.php
@@ -8,9 +8,7 @@ header('Location: login.php');
   echo "true";  
  }
 
-function twitter(){
-shell_exec("python3 Classification.py ".$handle." ".$_COOKIE['id'].";" );
-}
+
 ?>
 
 
@@ -81,8 +79,8 @@ shell_exec("python3 Classification.py ".$handle." ".$_COOKIE['id'].";" );
                   <h4>0</h4>
                 </div>
                 <div class="card-action blue">
-                   <form action="" method="POST">
-                   <input type="submit" name = "analyze" onclick="twitter();">
+                   <form action="twitter.php" method="POST">
+                   <input type="submit" name ="analyze">
                       </form>
                   <a href="" class="white-text">Link</a>
                 </div>

4e5fdd78f611b8df80755b407f848130be77e0af
Akash B <bobba.akash@gmail.com>
2018-11-03 18:57:14 -0400

    Update analysis.php

--- templates/analysis.php
+++ templates/analysis.php
@@ -81,7 +81,7 @@ shell_exec("python3 Classification.py ".$handle." ".$_COOKIE['id'].";" );
                   <h4>0</h4>
                 </div>
                 <div class="card-action blue">
-                   <form action="">
+                   <form action="" method="POST">
                    <input type="submit" name = "analyze">
                       </form>
                   <a href="" class="white-text">Link</a>

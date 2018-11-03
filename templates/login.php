d93a4552320b8927ac350640955f52527bc25b2a
Akash B <bobba.akash@gmail.com>
2018-11-03 14:20:26 -0400

    Update login.php

--- templates/login.php
+++ templates/login.php
@@ -4,12 +4,12 @@ $db = mysqli_connect('presence.cmw5zmeeepxi.us-east-1.rds.amazonaws.com', 'prese
     OR die(mysql_error());
 
 // Query
-$query = sprintf("SELECT * FROM users WHERE user='%s' AND password='%s'",
+$query = sprintf("SELECT * FROM user WHERE username='%s' AND password='%s'",
        mysqli_real_escape_string($_POST['username']),
        mysqli_real_escape_string($_POST['password']);
             
        $result = mysqli_query($db,$query);
-       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+       $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
       
        $count = mysqli_num_rows($result);
                  

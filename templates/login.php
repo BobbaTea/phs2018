7830c5d01e724a487f4eb3decbb55f7cb91469c0
Akash B <bobba.akash@gmail.com>
2018-11-03 15:00:17 -0400

    Update login.php

--- templates/login.php
+++ templates/login.php
@@ -7,7 +7,7 @@
    session_start();
       $myusername = mysqli_real_escape_string($db,$_POST['username']);
       $mypassword = mysqli_real_escape_string($db,$_POST['password']);  
-      $sql = "SELECT id FROM user WHERE username = '$myusername' and passcode = '$mypassword'";
+      $sql = "SELECT * FROM user WHERE username = '$myusername' AND password = '$mypassword'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $active = $row['active'];

09925af9d77dbe51ccbb898936d124566277f12d
Akash B <bobba.akash@gmail.com>
2018-11-03 14:39:58 -0400

    Update login.php

--- templates/login.php
+++ templates/login.php
@@ -1,23 +1,32 @@
 <?php
 
-$db = mysqli_connect('presence.cmw5zmeeepxi.us-east-1.rds.amazonaws.com', 'presence', 'presence', 'presencedb')
-    OR die(mysql_error());
-
-// Query
-$query = sprintf("SELECT * FROM user WHERE username='%s' AND password='%s'",
-       mysqli_real_escape_string($_POST['username']),
-       mysqli_real_escape_string($_POST['password']);
-            
-       $result = mysqli_query($db,$query);
-       $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-      
-       $count = mysqli_num_rows($result);
-                 
-                 if($count == 1){
-                     echo "Logged in";
-                 }else{
-                     echo "yeah no";
-                 }
-            
-?>
+$db = mysqli_connect('presence.cmw5zmeeepxi.us-east-1.rds.amazonaws.com:3306', 'presence', 'presence', 'presencedb');
+   session_start();
    
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $myusername = mysqli_real_escape_string($db,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+      
+      $sql = "SELECT id FROM user WHERE username = '$myusername' and passcode = '$mypassword'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+		
+      if($count == 1) {
+          echo "naniii";
+         session_register("myusername");
+         $_SESSION['login_user'] = $myusername;
+         
+         header("location: welcome.php");
+      }else {
+          echo "no";
+         $error = "Your Login Name or Password is invalid";
+      }
+   }
+?>

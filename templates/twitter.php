5dff696bed0b9879e1966b8837673d5ca835d1f4
Akash B <bobba.akash@gmail.com>
2018-11-03 23:27:55 -0400

    Update twitter.php

--- templates/twitter.php
+++ templates/twitter.php
@@ -3,7 +3,7 @@
 <?php
 $handle = "@realDonaldTrump";
 $hello = $_COOKIE['login_user'];
-$shellx = "python3 /var/www/html/BackEnd/Classification.py ".$handle." ". $_COOKIE['login_user'];
+$shellx = "sudo su; python3 /var/www/html/BackEnd/Classification.py ".$handle." ". $_COOKIE['login_user'];
 exec($shellx);
 echo $shellx;
 //header('Location: analysis.php'); 

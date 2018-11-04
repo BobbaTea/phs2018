f4592c5cbdd4b53357850c3eb183eed4dc9a2510
Akash B <bobba.akash@gmail.com>
2018-11-03 21:54:27 -0400

    Update twitter.php

--- templates/twitter.php
+++ templates/twitter.php
@@ -5,7 +5,7 @@ $hello = $_COOKIE['login_user'];
 $shellx = "python3 /var/www/html/BackEnd/Classification.py ".$handle." ". $_COOKIE['login_user'];
 exec($shellx);
 echo $shellx;
-//header('Location: analysis.php'); 
+header('Location: analysis.php'); 
 
 
 ?>

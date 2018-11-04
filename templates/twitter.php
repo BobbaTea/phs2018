2a9697875c261a716ac797bde1fcb679c7df8252
Samvit123 <samvitagarwal03@gmail.com>
2018-11-04 02:16:29 -0500

    Update twitter.php

--- templates/twitter.php
+++ templates/twitter.php
@@ -1,8 +1,8 @@
-
-<?php 
-
-$command = escapeshellcmd('/var/www/html/BackEnd/Classification.py @realDonaldTrump 1');
-$output = shell_exec($command);
-echo $output;
-
+<?php
+$handle = $_POST['handle'];
+$hello = $_COOKIE['login_user'];
+$shellx = "python3 /var/www/html/BackEnd/Classification.py ".$handle." ". $_COOKIE['login_user'];
+shell_exec($shellx);
+echo $shellx;
+//header('Location: analysis.php'); 
 ?>

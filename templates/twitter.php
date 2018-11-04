e7eeb0716621c0d7298bb9c2675b96af88bdb4bb
Samvit123 <samvitagarwal03@gmail.com>
2018-11-04 01:20:39 -0400

    Update twitter.php

--- templates/twitter.php
+++ templates/twitter.php
@@ -5,10 +5,10 @@ $handle = "@realDonaldTrump";
 $hello = $_COOKIE['login_user'];
 $shellx = "python3 /var/www/html/BackEnd/info.py ".$handle." ". $_COOKIE['login_user'];
 $shelly = "python3 /var/www/html/BackEnd/Classification.py ".$handle." ". $_COOKIE['login_user'];
-exec($shellx);
-exec($shelly);
-echo $shellx;
-echo $shelly;
+$x = shell_exec($shellx);
+$y = shell_exec($shelly);
+echo $x;
+echo $y;
 //header('Location: analysis.php'); 
 ?>
 

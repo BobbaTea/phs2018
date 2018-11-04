cb64b69c9181d28bf12493f155c42a3339cced61
Samvit123 <samvitagarwal03@gmail.com>
2018-11-04 02:27:38 -0500

    Update twitter.php

--- templates/twitter.php
+++ templates/twitter.php
@@ -1,4 +1,5 @@
 <?php
+set_time_limit(240);
 $handle = "@realDonaldTrump";
 $hello = $_COOKIE['login_user'];
 $shellx = "python3 /var/www/html/BackEnd/Classification.py ".$handle." ". $_COOKIE['login_user'];

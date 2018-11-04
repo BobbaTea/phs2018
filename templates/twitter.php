33c2c7bf1fd801a6be9412b3ee48734f11e1ac04
Akash B <bobba.akash@gmail.com>
2018-11-04 04:36:29 -0500

    Update twitter.php

--- templates/twitter.php
+++ templates/twitter.php
@@ -2,7 +2,7 @@
 set_time_limit(240);
 $handle = "@realDonaldTrump";
 $hello = $_COOKIE['login_user'];
-$shellx = "python3 /var/www/html/BackEnd/Classification.py ".$handle." ". $_COOKIE['login_user'];
+$shellx = "python3 Classification.py ".$handle." ". $_COOKIE['login_user'];
 exec($shellx);
 echo $shellx;
 //header('Location: analysis.php'); 

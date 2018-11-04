ba8fe22991f4d371f032ee8b390c6b4f00a1337f
Akash B <bobba.akash@gmail.com>
2018-11-04 05:03:49 -0500

    Update twitter.php

--- templates/twitter.php
+++ templates/twitter.php
@@ -1,14 +1,16 @@
 <?php
 set_time_limit(240);
 if (php_sapi_name() == "cli") {
-exec('php twitter.php');
-} else {
-  $handle = "@realDonaldTrump";
+    $handle = "@realDonaldTrump";
 $_COOKIE['login_user']=1;
 $shellx = "python3 Classification.py ".$handle." ". $_COOKIE['login_user'];
 exec($shellx);
 echo $shellx;
-    // Not in cli-mode
+} else {
+
+  exec('php twitter.php');
+
+  // Not in cli-mode
 }
 
 //header('Location: analysis.php'); 

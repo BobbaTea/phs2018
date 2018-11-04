b0384ab3484c39a80915978955b20d747454698f
Samvit123 <samvitagarwal03@gmail.com>
2018-11-03 20:43:28 -0400

    Update Classification.py

--- BackEnd/Classification.py
+++ BackEnd/Classification.py
@@ -17,7 +17,7 @@ from google.cloud.language import types
 hand = str(sys.argv[1])
 id = str(sys.argv[2])
 
-f=open("User"+str(id)+".txt","w+")
+f=open("User"+str(id),"w+")
 f.close()
 
 def analyze(handle):
@@ -33,13 +33,17 @@ def analyze(handle):
 
     full_text=""
 
-    with open("User"+str(id)+".txt","a+",encoding="utf-8") as w:
+    with open("User"+str(id),"a+",encoding="utf-8") as w:
         for status in statuses:
             status = (status.full_text)
             w.write(status)
             w.write("\n")
 
 analyze(str(hand))
+
+with open("User"+str(id),"a+",encoding="utf-8") as w:
+    w.write("\nTESTING")
+ 
 
 creds = service_account.Credentials.from_service_account_file(
     "/var/www/html/BackEnd/SocialEye-ca911f59a028.json")
@@ -73,7 +77,7 @@ def classify(text, verbose=True):
 
     return result
 
-with open("User"+str(id)+".txt","r") as o:
+with open("User"+str(id),"r") as o:
     r=o.readlines()
 
 i=0
@@ -135,7 +139,7 @@ percent_neut = str(round((float(neutral)/total)*100))
 f=open("User"+str(id)+".txt","w+")
 f.close()
 
-with open("User"+str(id)+".txt","a+") as w:
+with open("User"+str(id),"a+") as w:
     w.write("\n\nContent Distribution: ")
     w.write("\nPercent potentially offensive: "+percent_poffensive)
     w.write("\nPercent likely offensive: "+percent_loffensive)
